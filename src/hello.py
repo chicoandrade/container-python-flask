aee68fab3d494b5f7edc8b7498f1f6e38b528012
Francisco Andrade <deandradefj@gmail.com>
2017-09-15 14:34:40 -0400

    Trying something new

--- src/hello.py
+++ src/hello.py
@@ -1,24 +1,45 @@
 from decouple import config
-from flask import Flask, abort, request, jsonify, make_response
+from flask import Flask, abort, request, jsonify, make_response, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Well... Hello them!"
+    return "Well... Hello then!"
+
 
 @app.route('/hello_name', methods=['GET'])
 def hello_name():
-	if request.args:
-		return make_response(jsonify(message="Hello noname"))
-	else:
-		args = request.args.to_dict()
+    if len(request.args) < 1:
+        print("No arguments passed")
+        return make_response(jsonify(message="Hello noname"))
+    else:
+        args = request.args.to_dict()
 
-		if 'name' in args:
-			message = "Hello " + args['name']
-		else:
-			message = "Hello noname"
+        if 'name' in args:
+            if args['name'] == 'temer':
+                abort(make_response(jsonify(message="Sem golpe! Fora Temer"), 403))
 
-		return make_response(jsonify(message=message))
+            print("Arguments name: " + args['name'])
+            message = "Hello " + args['name']
+        else:
+            print("No argument name passed")
+            message = "Hello noname"
+
+        return make_response(jsonify(message=message))
+
+
+@app.route('/home')
+def home():
+    return render_template('home.html')
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    username = str(request.form['username'])
+    if username == 'temer':
+        abort(make_response(jsonify(message="Sem golpe! Fora Temer"), 403))
+    else:
+        return hello_name()
 
 
 if __name__ == '__main__':
